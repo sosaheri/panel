b4255dbda799bdac29266039ba55bdce1e9f3e3a
“Heriberto” <sosaheriberto2001@gmail.com>
2024-05-08 14:21:41 -0400

    fix style viewport height

--- src/components/upload.ts
+++ src/components/upload.ts
@@ -7,15 +7,14 @@ export function initUpload() {
     inputImagen.addEventListener('change', function(event) {
     if (event.target.files && event.target.files[0]) {
         
-
         
         const reader = new FileReader();
         reader.onload = function(event) {
-        imagenFondo.src = event.target.result;
+            imagenFondo.src = event.target.result;
         };
         reader.readAsDataURL(event.target.files[0]);
 
-        addLayer(event.target.files[0]);
+        // addLayer(event.target.files[0]);
         // removeLayer('layer-id');
         // updateLayerPosition('layer-id', 100, 50);
     }
